85815d0a642d18d0d469b116f6bc90058bae9e97
SergioNunFer <sernunfer@alu.edu.gva.es>
2025-04-07 18:22:44 +0200

    Web casi definitiva

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 
 menuIcon.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
+    navLinks.classList.toggle('show');
 });
 
 const links = document.querySelectorAll('.nav-links li a');
 
 links.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    const targetId = link.getAttribute('href').slice(1); // Obtiene el ID de la sección destino
+    link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').slice(1);
     const targetSection = document.getElementById(targetId);
     
     window.scrollTo({
-      top: targetSection.offsetTop,
-      behavior: 'smooth'
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
     });
 
-    // Cierra el menú al hacer clic en un enlace
     navLinks.classList.remove('show');
-  });
+    });
 });
